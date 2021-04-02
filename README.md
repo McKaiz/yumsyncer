35b76bd91e397ac61f102dc11d6ce7024d20a379
McKaiz <45264545+McKaiz@users.noreply.github.com>
2021-04-02 16:00:03 +0200

    Initial commit
    Signed-off-by: McKaiz <45264545+McKaiz@users.noreply.github.com>

---
+++ README.md
@@ -0,0 +1,24 @@
+# yumsyncer
+
+[![Build Status](https://travis-ci.com/McKaiz/yumsyncer.svg?branch=main)](https://travis-ci.com/McKaiz/yumsyncer)
+
+This container creates a local repositoty (without webserver for now) for the given .repo files in the repository, creates the repo with groups if given and waits for a rerun.
+
+## Directories
+
+`/data` => Destination for the synced repostory. Needs read/write access
+`/repo` => Source for the .repo files. Can be read-only
+
+## Environment Vars
+
+`waitDuration` => Time after sync how long to wait. Default: 12h
+`TZ` => Time Zone for the Container. Default: Europe/Amsterdam.
+
+## Example
+
+`docker run -d -v data:/data -v repo:/repo acrossthisnewdivide/yumsyncer`
+
+This container has no default `.repo`files.
+Make sure to take a look at this Readme before updating. This project is still under development so every update might break an existing container.
+
+WITHOUT ANY WARRANTY!
---
+++ script.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+##################################################################################
+#MIT License
+#
+#Copyright (c) 2021 Kai
+#
+#Permission is hereby granted, free of charge, to any person obtaining a copy
+#of this software and associated documentation files (the "Software"), to deal
+#in the Software without restriction, including without limitation the rights
+#to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#copies of the Software, and to permit persons to whom the Software is
+#furnished to do so, subject to the following conditions:
+#
+#The above copyright notice and this permission notice shall be included in all
+#copies or substantial portions of the Software.
+#
+#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+#SOFTWARE.
+##################################################################################
+
+DIR=/data/centos/
+while true; do
+    start=$(date +"%T")
+    echo "Starting sync @"$start
+    rm -f /etc/yum.repos.d/*
+    cp /repo/*.repo /etc/yum.repos.d/.
+    reposync -g -l -d -m --newest-only --download-metadata --download_path=${DIR}
+
+    for directory in $DIR/*/; do
+        if test -f $directory/comps.xml; then
+            cd $directory && createrepo -g comps.xml .
+        else
+            cd $directory && createrepo .
+        fi
+    done
+    end=$(date +"%T")
+    if ${oneTimeRun} == 0; then
+        echo "Done Downloading. Shutting Off!"
+        exit 0
+    fi
+
+    echo "Done Downloading! Waiting for" ${waitDuration} "(Current Time: " $end ")"
+    sleep ${waitDuration}
+done
